604a66fbae3e6763e91b7c21ed0e93128981b356
Ashley Rich <hello@ashleyrich.com>
2025-02-21 23:33:14 +0000

    Use vite

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
+
+export default defineConfig({
+    build: {
+        outDir: 'resources/dist',
+        lib: {
+            entry: resolve(__dirname, 'resources/src/beacon.js'),
+            name: 'DebugHawk',
+            fileName: 'beacon',
+            formats: ['iife']
+        },
+        rollupOptions: {
+            output: {
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`
+            }
+        }
+    }
+});
