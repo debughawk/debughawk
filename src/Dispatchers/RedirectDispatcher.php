0204a5934aeb6ba3bad980ca6add815e39f583ea
Ashley Rich <hello@ashleyrich.com>
2025-05-15 11:21:34 +0100

    Don't track /favicon.ico redirects

--- src/Dispatchers/RedirectDispatcher.php
+++ src/Dispatchers/RedirectDispatcher.php
@@ -14,6 +14,10 @@ class RedirectDispatcher extends Dispatcher implements NeedsInitiatingInterface 
 	}
 
 	public function send_redirect_metrics( $location ) {
+		if ( $this->is_ignored_uri() ) {
+			return $location;
+		}
+
 		$metrics = $this->gather_and_encrypt();
 
 		$args = apply_filters( 'debughawk_send_redirect_metrics_args', [
@@ -33,5 +37,13 @@ class RedirectDispatcher extends Dispatcher implements NeedsInitiatingInterface 
 
 	public function should_send_redirect_metrics() {
 		return apply_filters( 'debughawk_should_send_redirect_metrics', $this->config->is_within_sample_range() );
+	}
+
+	protected function is_ignored_uri(): bool {
+		$ignored_uris = apply_filters( 'debughawk_ignored_redirect_uris', [
+			'/favicon.ico',
+		] );
+
+		return in_array( $_SERVER['REQUEST_URI'], $ignored_uris, false );
 	}
 }
