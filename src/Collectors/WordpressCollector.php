1de08c266e7e872598e84c9b71e5cae4ce101e63
Ashley Rich <hello@ashleyrich.com>
2025-10-03 22:31:44 +0100

    Send plugin version

--- src/Collectors/WordpressCollector.php
+++ src/Collectors/WordpressCollector.php
@@ -9,14 +9,15 @@ class WordpressCollector extends Collector {
 		$user_id = get_current_user_id();
 
 		return [
-			'is_admin'      => is_admin(),
-			'is_front_page' => is_front_page(),
-			'is_search'     => is_search(),
-			'is_404'        => is_404(),
-			'post_id'       => is_singular() ? get_the_ID() : null,
-			'post_type'     => is_singular() ? get_post_type() : null,
-			'user_id'       => $user_id ?: null,
-			'version'       => get_bloginfo( 'version' ),
+			'is_admin'       => is_admin(),
+			'is_front_page'  => is_front_page(),
+			'is_search'      => is_search(),
+			'is_404'         => is_404(),
+			'post_id'        => is_singular() ? get_the_ID() : null,
+			'post_type'      => is_singular() ? get_post_type() : null,
+			'user_id'        => $user_id ?: null,
+			'version'        => get_bloginfo( 'version' ),
+			'plugin_version' => $this->config->version,
 		];
 	}
 }
